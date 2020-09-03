535339dd1d5cf4b4c9a09533ef0a474d980791b7
sumpson0-0 <sumpson00@gmail.com>
2020-09-03 20:31:52 +0900

    Delete News

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import home from '../routes/Home';
-import news from '../routes/News';
 import symptoms from '../routes/Symptoms';
 import dashboard from '../routes/Dashboard';
 
@@ -10,7 +9,6 @@ export default () => (
 		<>
 			<Switch>
 				<Route path="/" exact component={home} />
-				<Route path="/news" component={news} />
 				<Route path="/symptoms" component={symptoms} />
 				<Route path="/dashboard" component={dashboard} />
 				<Redirect from="*" to="/" />
