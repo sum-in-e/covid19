6e6be2628b414bcbde1fefd19b53002f918972a3
sumpson0-0 <sumpson00@gmail.com>
2020-09-08 14:57:54 +0900

    파일명 변경 (Symptoms => About)

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import home from '../routes/Home';
-import symptoms from '../routes/Symptoms';
+import about from '../routes/About';
 import dashboard from '../routes/Dashboard';
 
 export default () => (
@@ -9,7 +9,7 @@ export default () => (
 		<>
 			<Switch>
 				<Route path="/" exact component={home} />
-				<Route path="/about" component={symptoms} />
+				<Route path="/about" component={about} />
 				<Route path="/dashboard" component={dashboard} />
 				<Redirect from="*" to="/" />
 			</Switch>
--- src/routes/Symptoms.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Symptoms = () => {
-	return <h1>Symptoms</h1>;
-};
-
-export default Symptoms;
