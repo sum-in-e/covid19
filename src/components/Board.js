5d7bcb52ff6a49777181f9d94763183dfc15fc2d
sumpson0-0 <sumpson00@gmail.com>
2020-09-16 20:06:45 +0900

    Data 출력

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,44 +1,64 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Board = () => {
+const Board = ({ loading, data, error }) => {
+	/*
+	data.['Active Cases_text'],
+			data.Country_text,
+			data.['Last Update'],
+			data.['New Cases_text'],
+			data.['New Deaths_text'],
+			data.['Total Cases_text'],
+			data.['Total Deaths_text'],
+			data.['Total Recovered_text'],
+		);
+*/
 	return (
 		<Container>
-			<Title>S.Korea</Title>
-			<Results>
-				<Result>
-					<Total>
-						<H3>Coronavirus Cases</H3>
-						<TotalCount>0</TotalCount>
-					</Total>
-					<New>
-						<H4>New Cases</H4>
-						<NewCount>0</NewCount>
-					</New>
-				</Result>
-				<Result>
-					<Total>
-						<H3>Deaths</H3>
-						<TotalCount>0</TotalCount>
-					</Total>
-					<New>
-						<H4>New Deaths</H4>
-						<NewCount>0</NewCount>
-					</New>
-				</Result>
-				<Result>
-					<Total>
-						<H3>Recovered</H3>
-						<TotalCount>0</TotalCount>
-					</Total>
-				</Result>
-				<Result>
-					<Total>
-						<H3>Active Cases</H3>
-						<TotalCount>0</TotalCount>
-					</Total>
-				</Result>
-			</Results>
+			{data && Object.keys(data).length > 0 && (
+				<>
+					<Header>
+						<Title>{data.Country_text ? data.Country_text : '-'}</Title>
+						<LastUpdate>Last Update : {data['Last Update'] ? data['Last Update'] : '-'}</LastUpdate>
+					</Header>
+					<Results>
+						<Result>
+							<Total>
+								<H3>Coronavirus Cases</H3>
+								<TotalCount>{data['Total Cases_text'] ? data['Total Cases_text'] : '-'}</TotalCount>
+							</Total>
+							<New>
+								<H4>New Cases</H4>
+								<NewCount>{data['New Cases_text'] ? data['New Cases_text'] : '-'}</NewCount>
+							</New>
+						</Result>
+						<Result>
+							<Total>
+								<H3>Deaths</H3>
+								<TotalCount>{data['Total Deaths_text'] ? data['Total Deaths_text'] : '-'}</TotalCount>
+							</Total>
+							<New>
+								<H4>New Deaths</H4>
+								<NewCount>{data['New Deaths_text'] ? data['New Deaths_text'] : '-'}</NewCount>
+							</New>
+						</Result>
+						<Result>
+							<Total>
+								<H3>Recovered</H3>
+								<TotalCount>
+									{data['Total Recovered_text'] ? data['Total Recovered_text'] : '-'}
+								</TotalCount>
+							</Total>
+						</Result>
+						<Result>
+							<Total>
+								<H3>Active Cases</H3>
+								<TotalCount>{data['Active Cases_text'] ? data['Active Cases_text'] : '-'}</TotalCount>
+							</Total>
+						</Result>
+					</Results>
+				</>
+			)}
 		</Container>
 	);
 };
@@ -47,14 +67,23 @@ const Container = styled.section`
 	width: 100%;
 	height: 35vh;
 	padding: 1.5rem;
-	margin-bottom: 2rem;
-	background-color: rgba(7, 12, 50, 0.8);
+	margin-bottom: 2.5rem;
+	background-color: #1e2033;
 	border-radius: 25px;
 `;
 
-const Title = styled.h2`
+const Header = styled.header`
+	display: flex;
+	justify-content: space-between;
 	margin-bottom: 1.5rem;
+`;
+
+const Title = styled.h2`
 	font-size: 1.5rem;
+`;
+
+const LastUpdate = styled.span`
+	color: gray;
 `;
 
 const Results = styled.article`
@@ -82,7 +111,7 @@ const Results = styled.article`
 
 const Result = styled.div`
 	padding: 1rem;
-	border-left: dotted 1px #21252a;
+	border-left: dotted 1px #3e3f51;
 `;
 
 const Total = styled.div`
@@ -96,7 +125,7 @@ const H3 = styled.h3`
 `;
 
 const TotalCount = styled.span`
-	font-size: 2.5rem;
+	font-size: 2.2rem;
 	font-weight: bold;
 `;
 
@@ -109,7 +138,7 @@ const H4 = styled.h4`
 `;
 
 const NewCount = styled.span`
-	font-size: 1.2rem;
+	font-size: 1.1rem;
 `;
 
 export default Board;
