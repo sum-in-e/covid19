b5eeffcc8e93c4ab232b6cbbc99ea0c58d516889
sumpson0-0 <sumpson00@gmail.com>
2020-09-17 21:40:13 +0900

    Update api.js

--- src/api.js
+++ src/api.js
@@ -38,24 +38,3 @@ const useAxios = (options, axiosInstance = defaultAxios) => {
 };
 
 export default useAxios;
-
-/*
-import axios from 'axios';
-
-const covidApi = {
-	worldStatus: () =>
-		axios.get('/v1', {
-			headers: {
-				'x-rapidapi-host': 'covid-19-tracking.p.rapidapi.com',
-				'x-rapidapi-key': process.env.REACT_APP_API_KEY,
-			},
-		}),
-	countryStatus: country =>
-		axios.get(`/v1/${country}`, {
-			headers: {
-				'x-rapidapi-host': 'covid-19-tracking.p.rapidapi.com',
-				'x-rapidapi-key': process.env.REACT_APP_API_KEY,
-			},
-		}),
-};
-*/
