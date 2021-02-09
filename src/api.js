22c566480bf978b12d0f4bae5bd45127aeac1532
Kim Sumin <66582899+sumpson0-0@users.noreply.github.com>
2021-02-09 14:12:28 +0900

    Proxy 제거

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 const api = country => {
 	return Axios.request({
-		url: `/v1${country}`,
+		url: `https://covid-19-tracking.p.rapidapi.com/v1${country}`,
 		headers: {
 			'x-rapidapi-host': 'covid-19-tracking.p.rapidapi.com',
 			'x-rapidapi-key': process.env.REACT_APP_API_KEY,
